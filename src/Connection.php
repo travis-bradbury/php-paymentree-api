6db11067edfcf195250b4f55f1353876606ad18c
tbradbury <tbradbury@acromediainc.com>
2016-05-20 15:43:10 -0700

    Remove guzzle dependency.

--- src/Connection.php
+++ src/Connection.php
@@ -5,19 +5,48 @@ namespace Paymentree;
 class Connection {
 
   /**
+   * @var string
+   *   The ip address or host name to connect to.
+   */
+  protected $host;
+
+  /**
+   * @var int
+   *   The port to connect to.
+   */
+  protected $port;
+
+  /**
    * Connection constructor.
-   * @param string $ip_address
+   * @param string $host
    * @param int $port
    */
-  public function __construct($ip_address, $port) {
-
+  public function __construct($host, $port) {
+    $this->host = $host;
+    $this->port = $port;
   }
 
   /**
    * @param string $data
    * @return string
+   * @throws \Exception
    */
-  public function send($data) {
-    return '<TRANS><PAYLinqTransId>1520913221307</PAYLinqTransId><CheckNo>4717</CheckNo></TRANS>';
+  public function send($data = '') {
+    $response = '';
+
+    $data = '<TRANS><PAYLinqTransId>1520913221307</PAYLinqTransId><CheckNo>4717</CheckNo></TRANS>';
+    $errno = NULL;
+    $errstr = NULL;
+    if ($resource = stream_socket_client('tcp://' . $this->host . ':' . $this->port, $errno, $errstr)) {
+      fwrite($resource, $data);
+      $response = fgets($resource);
+      fclose($resource);
+    }
+
+    if (!empty($errno) || !empty($errstr)) {
+      throw new \Exception($errno . ': ' . $errstr);
+    }
+
+    return $response;
   }
 }
