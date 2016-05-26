06b58fb40e65367c95c67a9664b1fd80f4246635
tbradbury <tbradbury@acromediainc.com>
2016-05-26 16:18:28 -0700

    Add getters and setters to PaymentRefundTransaction.

--- src/PaymentRefundTransaction.php
+++ src/PaymentRefundTransaction.php
@@ -60,6 +60,102 @@ class PaymentRefundTransaction extends Transaction {
   }
 
   /**
+   * @return string
+   */
+  public function getClient() {
+    return $this->client;
+  }
+
+  /**
+   * @param string $client_id
+   * @return $this
+   */
+  public function setClient($client_id) {
+    $this->client = $client_id;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getLocation() {
+    return $this->location;
+  }
+
+  /**
+   * @param string $location
+   * @return $this
+   */
+  public function setLocation($location) {
+    $this->location = $location;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getCashier() {
+    return $this->cashier;
+  }
+
+  /**
+   * @param string $cashier
+   * @return $this
+   */
+  public function setCashier($cashier) {
+    $this->cashier = $cashier;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getReqTransId() {
+    return $this->req_trans_id;
+  }
+
+  /**
+   * @param string $id
+   * @return $this
+   */
+  public function setReqTransId($id) {
+    $this->req_trans_id = $id;
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getAmount() {
+    return $this->amount;
+  }
+
+  /**
+   * @param int $amount
+   * @return $this
+   */
+  public function setAmount($amount) {
+    $this->amount = $amount;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getToken() {
+    return $this->token;
+  }
+
+  /**
+   * @param string $token
+   * @return $this
+   */
+  public function setToken($token) {
+    $this->token = $token;
+    return $this;
+  }
+
+  /**
    * @param \DOMDocument|NULL $document
    * @return \DOMDocument
    */
