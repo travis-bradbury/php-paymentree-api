b2fd2d89ebe0f6945c1227926af535b0e10a4e29
tbradbury <tbradbury@acromediainc.com>
2016-06-01 15:13:12 -0700

    Add PHPDoc to ResponseInterface.

--- src/ResponseInterface.php
+++ src/ResponseInterface.php
@@ -6,16 +6,64 @@ use \DOMDocument;
 
 interface ResponseInterface {
   public function __construct(DOMDocument $document);
+
+  /**
+   * @return string
+   */
   public function getType();
+
+  /**
+   * @return string
+   */
   public function getPayLinqTransactionId();
+
+  /**
+   * @return \DOMDocument
+   */
   public function getDocument();
+
+  /**
+   * @return string
+   */
   public function getChequeNumber();
+
+  /**
+   * @return string
+   */
   public function getResponseCode();
+
+  /**
+   * @return string
+   */
   public function getResponseMessage();
+
+  /**
+   * @return string
+   */
   public function getCardType();
+
+  /**
+   * @return string
+   */
   public function getProcessedAmount();
+
+  /**
+   * @return string
+   */
   public function getCardCode();
+
+  /**
+   * @return string
+   */
   public function getMerchantReceipt();
+
+  /**
+   * @return string
+   */
   public function getCustomerReceipt();
+
+  /**
+   * @return string
+   */
   public function getTransactionCode();
 }
